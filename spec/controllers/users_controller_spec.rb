4d9ca5580dc118ee02b26f90e692a7affaf97944
David Kerins <dkerins@bitbox.ca>
2010-12-19 12:24:07 -0800

    Completed  sign-in and sign-out

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -110,9 +110,15 @@ describe UsersController do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end    
+      
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to twitter-like/i
+      end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
     end
   end
